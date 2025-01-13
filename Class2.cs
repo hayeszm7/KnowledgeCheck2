66c57c9c883bb73e7fbb7e32caf7b817ce4c811d
Zac Hayes <hayeszm7@gmail.com>
2025-01-12 19:47:29 -0500

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    public class Grades : Student
+    {
+        public string Grade {  get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using KnowledgeCheck2;
+using System.Data.Common;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Grades>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var myClass = new Grades();
+
+    Console.WriteLine("Enter the students name");
+    myClass.Name = Console.ReadLine();
+    
+    Console.WriteLine("Enter the students grade from A-F");
+    myClass.Grade = Console.ReadLine();
+    recordList.Add(myClass);
+}
+
+// Print out the list of records using Console.WriteLine()
+foreach (var record in recordList)
+{
+    Console.WriteLine($"{record.Name} recieved a grade of letter {record.Grade}");
+}
+
+
